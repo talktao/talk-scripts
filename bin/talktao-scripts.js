2f8202cffb20c4e7d434472214d83f54ef81a0b3
tantao <t1416249906@163.com>
2023-04-14 15:40:12 +0800

    fix

--- bin/talktao-scripts.js
+++ bin/talktao-scripts.js
@@ -36,14 +36,14 @@ if (['build', 'eject', 'start', 'test'].includes(script)) {
     if (result.signal === 'SIGKILL') {
       console.log(
         'The build failed because the process exited too early. ' +
-          'This probably means the system ran out of memory or someone called ' +
-          '`kill -9` on the process.'
+        'This probably means the system ran out of memory or someone called ' +
+        '`kill -9` on the process.'
       );
     } else if (result.signal === 'SIGTERM') {
       console.log(
         'The build failed because the process exited too early. ' +
-          'Someone might have called `kill` or `killall`, or the system could ' +
-          'be shutting down.'
+        'Someone might have called `kill` or `killall`, or the system could ' +
+        'be shutting down.'
       );
     }
     process.exit(1);
@@ -53,6 +53,6 @@ if (['build', 'eject', 'start', 'test'].includes(script)) {
   console.log('Unknown script "' + script + '".');
   console.log('Perhaps you need to update react-scripts?');
   console.log(
-    'See: https://facebook.github.io/create-react-app/docs/updating-to-new-releases'
+    'See: https://github.com/talktao/talktao-scripts'
   );
 }
