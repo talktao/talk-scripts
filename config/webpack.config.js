aca9e9d521ebdd7cf2f67ebdb588f32478669aaa
tantao <t1416249906@163.com>
2023-05-11 14:32:05 +0800

    fix: error message of 'unexpected character '�'

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -374,6 +374,14 @@ module.exports = function (webpackEnv) {
                 },
               },
             },
+            // Fix the error message of 'unexpected character '�' ' when introducing local images in the development environment
+            {
+              test: /\.(png|svg|webp|jpe?g)$/i,
+              loader: 'url-loader',
+              options: {
+                esModule: false
+              }
+            },
             {
               test: /\.path\.svg$/,
               use: [
@@ -435,7 +443,7 @@ module.exports = function (webpackEnv) {
                         },
                       ],
                     ],
-    
+
                     plugins: [
                       isEnvDevelopment &&
                       shouldUseReactRefresh &&
@@ -477,7 +485,7 @@ module.exports = function (webpackEnv) {
                     cacheDirectory: true,
                     // See #6846 for context on why cacheCompression is disabled
                     cacheCompression: false,
-    
+
                     // Babel sourcemaps are needed for debugging into node_modules
                     // code.  Without the options below, debuggers like VSCode
                     // show incorrect code and set breakpoints on the wrong lines.
