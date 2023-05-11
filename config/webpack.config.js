c6e6adb4751d915cd0ce819a85b46567fb76593b
tantao <t1416249906@163.com>
2023-05-11 18:26:59 +0800

    fix: AutomaticPrefetchPlugin 导致项目开发时，import 图片错误

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,11 @@
+// @remove-on-eject-begin
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+// @remove-on-eject-end
 'use strict';
 
 const fs = require('fs');
@@ -246,9 +254,6 @@ module.exports = function (webpackEnv) {
       level: 'none',
     },
     optimization: {
-      // splitChunks: isEnvProduction ? {
-      //   chunks: "all",
-      // } : {},
       minimize: isEnvProduction,
       minimizer: [
         // This is only used in production mode
@@ -409,52 +414,41 @@ module.exports = function (webpackEnv) {
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
               include: paths.appSrc,
-              loader: require.resolve('babel-loader'),
-              options: {
-                customize: require.resolve(
-                  'babel-preset-react-app/webpack-overrides'
-                ),
-                presets: [
-                  [
-                    require.resolve('babel-preset-react-app'),
-                    {
-                      runtime: hasJsxRuntime ? 'automatic' : 'classic',
-                    },
-                  ],
-                ],
-                // @remove-on-eject-begin
-                babelrc: false,
-                configFile: false,
-                // Make sure we have a unique cache identifier, erring on the
-                // side of caution.
-                // We remove this when the user ejects because the default
-                // is sane and uses Babel options. Instead of options, we use
-                // the react-scripts and babel-preset-react-app versions.
-                cacheIdentifier: getCacheIdentifier(
-                  isEnvProduction
-                    ? 'production'
-                    : isEnvDevelopment && 'development',
-                  [
-                    'babel-plugin-named-asset-import',
-                    'babel-preset-react-app',
-                    'react-dev-utils',
-                    'react-scripts',
-                  ]
-                ),
-                // @remove-on-eject-end
-                plugins: [
-                  isEnvDevelopment &&
-                  shouldUseReactRefresh &&
-                  require.resolve('react-refresh/babel'),
-                ].filter(Boolean),
-                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                // It enables caching results in ./node_modules/.cache/babel-loader/
-                // directory for faster rebuilds.
-                cacheDirectory: true,
-                // See #6846 for context on why cacheCompression is disabled
-                cacheCompression: false,
-                compact: isEnvProduction,
-              },
+              use: [
+                {
+                  loader: "thread-loader",
+                  options: { workers: os.cpus().length }
+                },
+                {
+                  loader: "babel-loader",
+                  options: {
+                    customize: require.resolve(
+                      'babel-preset-react-app/webpack-overrides'
+                    ),
+                    presets: [
+                      [
+                        require.resolve('babel-preset-react-app'),
+                        {
+                          runtime: hasJsxRuntime ? 'automatic' : 'classic',
+                        },
+                      ],
+                    ],
+
+                    plugins: [
+                      isEnvDevelopment &&
+                      shouldUseReactRefresh &&
+                      require.resolve('react-refresh/babel'),
+                    ].filter(Boolean),
+                    // This is a feature of `babel-loader` for webpack (not Babel itself).
+                    // It enables caching results in ./node_modules/.cache/babel-loader/
+                    // directory for faster rebuilds.
+                    cacheDirectory: true,
+                    // See #6846 for context on why cacheCompression is disabled
+                    cacheCompression: false,
+                    compact: isEnvProduction,
+                  },
+                },
+              ],
             },
             // Process any JS outside of the app with Babel.
             // Unlike the application JS, we only compile the standard ES features.
@@ -535,9 +529,9 @@ module.exports = function (webpackEnv) {
                 },
               }),
             },
-            // Opt-in support for SASS (using .scss or .sass extensions).
-            // By default we support SASS Modules with the
-            // extensions .module.scss or .module.sass
+            // Opt-in support for LeSS (using .scss or .less extensions).
+            // By default we support LESS Modules with the
+            // extensions .module.scss or .module.less
             {
               test: lessRegex,
               exclude: lessModuleRegex,
@@ -559,8 +553,8 @@ module.exports = function (webpackEnv) {
               // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true,
             },
-            // Adds support for CSS Modules, but using SASS
-            // using the extension .module.scss or .module.sass
+            // Adds support for CSS Modules, but using LESS
+            // using the extension .module.scss or .module.less
             {
               test: lessModuleRegex,
               use: getStyleLoaders(
@@ -603,7 +597,6 @@ module.exports = function (webpackEnv) {
         format: `${chalk.cyan('Building')} [ ${chalk.cyan(':bar')} ]  ${chalk.bold('(:percent)')}`,
         clear: false
       }),
-      new webpack.AutomaticPrefetchPlugin(),
       ifAnalyzing && new BundleAnalyzerPlugin(),
       new WebpackHtmlCdnPlugin({
         modules: getCDNModules()
